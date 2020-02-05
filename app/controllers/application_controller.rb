b6a60aa99e3ec330af9b8ff59075e4bd70082da9
hiro-shy <do-not-forget-1220@hotmail.co.jp>
2020-02-05 19:42:14 +0900

    Basic認証の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :basic_auth
+
+  private
+
+  # ID・パスワードは暫定です
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'teckexpert62c' && password == '1234'
+    end
+  end
 end
