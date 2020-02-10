e05f6e764c3d9b07b432dc1fc8ea427f51723bb2
hiro-shy <do-not-forget-1220@hotmail.co.jp>
2020-02-10 21:55:40 +0900

    edit deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '<13.114.29.185>', user: 'ec2-user', roles: %w{app db web}
+server '13.114.29.185', user: 'ec2-user', roles: %w{app db web}
 
 # role-based syntax
 # ==================
