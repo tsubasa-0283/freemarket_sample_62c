7f85204ce8d50d94b285202a37329148d510d9de
hiro-shy <do-not-forget-1220@hotmail.co.jp>
2020-02-10 17:30:47 +0900

    edit unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -17,10 +17,10 @@ working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen 3000
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 
 #通常のログを記録するファイルを指定
 stdout_path "#{app_path}/log/unicorn.stdout.log"
