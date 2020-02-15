64f4ae0bb6d2b0beb06e64658f915f85a7c9cc46
amemy0504 <amemy050401@gmail.com>
2020-02-15 20:31:13 +0900

    aa

--- db/migrate/20200215102114_create_images.rb
+++ db/migrate/20200215102114_create_images.rb
@@ -1,8 +1,8 @@
 class CreateImages < ActiveRecord::Migration[5.0]
   def change
     create_table :images do |t|
-      t.string :src
-      t.references :product, foreign_key: true
+      t.string :image     null: false,
+      t.references :item, foreign_key: true
       t.timestamps
     end
   end
