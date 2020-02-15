c9163304b6ed2f667d08ed594b97f95c29ff7c99
tera-hana <htera711@vmail.plala.or.jp>
2020-02-15 19:33:28 +0900

    routing設定を戻す

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  root "toppage#index"
+  devise_for :installs
+  devise_for :users
+#   root "toppage#index"
+#   get 'toppage/index'
+  get 'users/new'
+  get 'deteils/index'
+  # get 'toppage/index'
+  # root "toppage#index"
+  root "deteils#index"
 end
-
