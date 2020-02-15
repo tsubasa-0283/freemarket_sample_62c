ee529efebc313c32c5d1fb9012ff0f5f7cd1c6ae
tera-hana <htera711@vmail.plala.or.jp>
2020-02-15 17:46:39 +0900

    不要なコメントを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :installs
-  devise_for :users
-#   get 'toppage/index'
-  get 'users/new'
-  # get 'deteils/index'
   root "toppage#index"
-  # root "deteils#index"
 end
 
