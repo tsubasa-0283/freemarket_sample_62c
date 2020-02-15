f04510086772091fd6465bb56e01e4283217730b
tsubasa-0283 <tsubasaokamoto7@gmail.com>
2020-02-15 15:35:47 +0900

    toppage root changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :installs
   devise_for :users
-#   root "toppage#index"
 #   get 'toppage/index'
   get 'users/new'
-  get 'deteils/index'
-  # get 'toppage/index'
-  # root "toppage#index"
-  root "deteils#index"
+  # get 'deteils/index'
+  root "toppage#index"
+  # root "deteils#index"
 end
 
