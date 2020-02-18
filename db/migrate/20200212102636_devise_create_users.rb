a60c34c9d70fab70fa3127464af5079a9c7e0c28
hiro-shy <do-not-forget-1220@hotmail.co.jp>
2020-02-18 18:24:32 +0900

    first_commit

--- db/migrate/20200212102636_devise_create_users.rb
+++ db/migrate/20200212102636_devise_create_users.rb
@@ -4,14 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,               null: false
-      t.string :email,                  null: false
-      t.string :password,               null: false
-      t.string :first_name,             null: false
-      t.string :last_name,              null: false
-      t.string :first_name_kana,        null: false
-      t.string :last_name_kana,         null: false
-      t.integer :tel,                   null: false
+      t.string :nickname,               null: false,limit: 20
+      t.string :email,                  null: false,unique: true 
+      t.string :password,               null: false,unique: true 
+      t.string :first_name,             null: false,limit: 30
+      t.string :last_name,              null: false,limit: 30
+      t.string :first_name_kana,        null: false,limit: 30
+      t.string :last_name_kana,         null: false,limit: 30
+      t.integer :tel,                   null: false,unique: true,limit: 30
       t.integer :birth_year            
       t.integer :birth_month           
       t.integer :birth_day             
