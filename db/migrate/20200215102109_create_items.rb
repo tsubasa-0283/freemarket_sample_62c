43ae95f44085595d7dbd28eed17d1e7ccacb7e52
tera-hana <htera711@vmail.plala.or.jp>
2020-02-22 17:07:43 +0900

    create item-table column

--- db/migrate/20200215102109_create_items.rb
+++ db/migrate/20200215102109_create_items.rb
@@ -1,7 +1,18 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
-
+      t.string :name,                 null: false
+      t.text :description,            null: false
+      t.integer :condition_id,        null: false
+      t.references :user_id,          null: false, foreign_key: true
+      t.integer :postage_id,          null: false, foreign_key: true
+      t.string :prefecture,           null: false
+      t.integer :delivery_day_id      null: false
+      t.integer :price                null: false
+      t.integer :brand_id             null: false
+      t.integer :likes_count          null: false
+      t.references :category_id       null: false , foreign_key: true
+      t.references :item_state_id     null: false , foreign_key: true
       t.timestamps
     end
   end
