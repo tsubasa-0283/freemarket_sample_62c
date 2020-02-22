015aeddcfca9878d10a64c399813ce469d202637
tera-hana <htera711@vmail.plala.or.jp>
2020-02-22 15:37:20 +0900

    商品一覧設定準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   devise_for :installs
   devise_for :users
-#   root "toppage#index"
-#   get 'toppage/index'
+  root "toppage#index"
+  get 'toppage/index'
   get 'users/new'
   get 'deteils/index'
-  # get 'toppage/index'
-  # root "toppage#index"
+  get 'toppage/index'
+  root "toppage#index"
   root "deteils#index"
 
   root 'items#index'
