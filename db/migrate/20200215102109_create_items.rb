b4f4b2d4e665b1ea5e54590817259cadf16adeaa
tera-hana <htera711@vmail.plala.or.jp>
2020-02-22 17:18:34 +0900

    item colmun fix

--- db/migrate/20200215102109_create_items.rb
+++ db/migrate/20200215102109_create_items.rb
@@ -1,12 +1,12 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
-      t.string :name,                 null: false
-      t.text :description,            null: false
-      t.integer :condition_id,        null: false
-      t.references :user_id,          null: false, foreign_key: true
-      t.integer :postage_id,          null: false, foreign_key: true
-      t.string :prefecture,           null: false
+      t.string :name                  null: false
+      t.text :description             null: false
+      t.integer :condition_id         null: false
+      t.references :user_id           null: false, foreign_key: true
+      t.integer :postage_id           null: false, foreign_key: true
+      t.string :prefecture            null: false
       t.integer :delivery_day_id      null: false
       t.integer :price                null: false
       t.integer :brand_id             null: false
