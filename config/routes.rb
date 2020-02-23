53ace9b6dfd3b6f38d1a72cb54d37dd36a5962d7
tsubasa-0283 <tsubasaokamoto7@gmail.com>
2020-02-23 23:10:29 +0900

    signup,loginpage create

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :installs
   devise_for :users
 #   root "toppage#index"
 #   get 'toppage/index'
   get 'users/new'
-  get 'deteils/index'
+  # get 'deteils/index'
   # get 'toppage/index'
   # root "toppage#index"
   root "deteils#index"
