79334ba55ed5e24cb1fdf4a910a296f6d146e6cc
mitsuhiro0602 <0hornet0@gmail.com>
2020-02-24 21:05:07 +0900

    コンフリクト修正

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,5 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200215102114) do
+ActiveRecord::Schema.define(version: 20200224064837) do
 
   create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "src"
@@ -35,8 +24,22 @@ ActiveRecord::Schema.define(version: 20200215102114) do
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.string   "prefecture",                    null: false
+    t.text     "description",     limit: 65535, null: false
+    t.integer  "condition_id",                  null: false
+    t.integer  "delivery_day_id",               null: false
+    t.integer  "brand_id",                      null: false
+    t.integer  "likes_count",                   null: false
+    t.integer  "user_id",                       null: false
+    t.integer  "category_id",                   null: false
+    t.integer  "item_state_id",                 null: false
+    t.integer  "postage_id",                    null: false
+    t.index ["category_id"], name: "index_items_on_category_id", using: :btree
+    t.index ["item_state_id"], name: "index_items_on_item_state_id", using: :btree
+    t.index ["postage_id"], name: "index_items_on_postage_id", using: :btree
+    t.index ["user_id"], name: "index_items_on_user_id", using: :btree
   end
 
   create_table "pictures", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
