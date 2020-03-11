9ae3e8aef52052d051129aad40e183477c3c6259
mitsuhiro0602 <0hornet0@gmail.com>
2020-03-11 13:44:32 +0900

    複数画像実装

--- app/assets/javascripts/error_message.js
+++ app/assets/javascripts/error_message.js
@@ -1,37 +1,37 @@
-(function(factory) {
-    if(type define === "function" && define.amd) {
-        define( ["jquery", "../jquery.validate"], factory);
-    }
-    else if (typeof module === "object" && module.exports) {
-        module.exports = factory(require("jequery"));
-    }
-    else {
-        factory(jQuery);
-    }
-}(function($) {
-    $.extend($.validator.messages, {
-        image: "画像がありません",
-        required: "入力してください",
-        remote: "このフィールドを修正してください",
-        email: "有効なメールアドレスを入力してください",
-        url: "有効なURLを入力してください",
-        date: "有効な日付を入力してください"
-        number: "300以上9999999以下で入力してください",
-        digits: "数字のみを入力してください",
-        creditcard: "有効なクレジットカード番号を入力してください",
-        equalTo: "同じ値をもう一度入力してください",
-        extension: "有効な拡張子を含む値を入力してください",
-        maxlength: $.validator.format( "{0} 文字以内で入力してください"),
-        minlength: $.validator.format( "{0} 円以上で入力してください"),
-        rangelength: $.validator.format("{0} 文字から {1} 文字までの値を入力してください"),
-        range: $.validator.format("{0} から　{1} までの値を入力してください"),
-        step: $.validator.format("{0} の倍数を入力してください"),
-        max: $.validator.format("40文字以下の値を入力してください"),
-        text_max: $.validator.format("1000文字以下の値を入力してください"),
-        min: $.validator.format( "{0} 以上の値を入力してください"),
-        choice: "選択してください",
-        price: $.validator.format("300円以上9999999以下で入力してください")
+// (function(factory) {
+//     if(type define === "function" && define.amd) {
+//         define( ["jquery", "../jquery.validate"], factory);
+//     }
+//     else if (typeof module === "object" && module.exports) {
+//         module.exports = factory(require("jequery"));
+//     }
+//     else {
+//         factory(jQuery);
+//     }
+// }(function($) {
+//     $.extend($.validator.messages, {
+//         image: "画像がありません",
+//         required: "入力してください",
+//         remote: "このフィールドを修正してください",
+//         email: "有効なメールアドレスを入力してください",
+//         url: "有効なURLを入力してください",
+//         date: "有効な日付を入力してください"
+//         number: "300以上9999999以下で入力してください",
+//         digits: "数字のみを入力してください",
+//         creditcard: "有効なクレジットカード番号を入力してください",
+//         equalTo: "同じ値をもう一度入力してください",
+//         extension: "有効な拡張子を含む値を入力してください",
+//         maxlength: $.validator.format( "{0} 文字以内で入力してください"),
+//         minlength: $.validator.format( "{0} 円以上で入力してください"),
+//         rangelength: $.validator.format("{0} 文字から {1} 文字までの値を入力してください"),
+//         range: $.validator.format("{0} から　{1} までの値を入力してください"),
+//         step: $.validator.format("{0} の倍数を入力してください"),
+//         max: $.validator.format("40文字以下の値を入力してください"),
+//         text_max: $.validator.format("1000文字以下の値を入力してください"),
+//         min: $.validator.format( "{0} 以上の値を入力してください"),
+//         choice: "選択してください",
+//         price: $.validator.format("300円以上9999999以下で入力してください")
 
-    });
-    return $;
-}));
+//     });
+//     return $;
+// }));
---
+++ app/assets/javascripts/item_new.js
@@ -0,0 +1,53 @@
+$(function(){
+    // DataTransferオブジェクトでデータを格納する箱を作る
+    var dataBox = new DataTransfer();
+    // querySelectorfile_fieldを取得する
+    var file_field = document.querySelector('input[type=file]')
+    // fileが選択された時に発火するイベント
+    $('#img-file').change(function() {
+        // 選択したfileのオブジェクトをpropで取得する
+        var files = $('input[type="file"]').prop('files')[0];
+        $.each(this.files, function(i, file){
+            // FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
+            var fileReader = new FileReader();
+
+            // DataTransferオブジェクトに対して、fileを追加する
+            dataBox.items.add(file)
+            // Datatransferオブジェクトに入ったfile一覧をfile_fieldの中に代入する
+            file_field.files = dataBox.files
+
+            var num = $('.item-image').length + 1 + i
+            fileReader.readAsDataURL(file);
+            // 画像が10枚になり、超えたらドロップボックスを削除する
+            if (num == 10){
+                $('#image-box__container').css('display', 'none')
+            }
+            // 読み込みが完了するとsrcにfileのURLを格納する
+            fileReader.onloadend = function(){
+                var src = fileReader.result
+                var html = `<div class='item-image' data-image="${file.src}">
+                                <div class=' item-iamge__content'>
+                                    <div class='item-image__content--icon'>
+                                        <img src=${src} width="114" height="80" >
+                                    </div>
+                                </div>
+                                <div class='item-image__operetion'>
+                                    <div class='item-image__operetion--delete'>削除</div>
+                                </div>
+                            </div>`
+                $('#image-box__container').before(html);
+            };
+            // image-box__containerのクラスを変更し、CSSでドロップボックスの大きさを変えてやる。
+            $('image-box__container').attr('class', `item-num-${num}`)
+        });
+    });
+    //削除ボタンをクリックすると発火するイベント
+    $(document).on("click", '.item-image__operetion--delete', function(){
+        // プレビュー要素を取得する
+        var target_image = $(this).parent().parent()
+        // プレビューを削除する
+        target_image.remove();
+        // inputタグに入ったファイルを削除する
+        file_field.val("")
+    })
+});
