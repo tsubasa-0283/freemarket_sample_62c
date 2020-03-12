e24e6a3d4f3cd0d71d4828097f3c598bf931febf
tera-hana <htera711@vmail.plala.or.jp>
2020-03-13 00:47:37 +0900

    商品一覧_サーバサイド
    コントローラ編集
    ルーティング編集
    haml編集
    画像追加

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,8 +1,10 @@
 class ToppagesController < ApplicationController
   def index
+
     @parents = []
     Category.where(ancestry: nil).each do |parent|
       @parents << parent.name
+    @items = Item.limit(9).includes(:images).order("created_at DESC")
     end
   end
 end
