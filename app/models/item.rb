0107a37771dc20fbea959de4d044e0096433640a
mitsuhiro0602 <0hornet0@gmail.com>
2020-03-16 03:48:05 +0900

    バリデーション完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+
+    # アソシエーション
+    extend ActiveHash::Associations::ActiveRecordExtensions
     has_many :likes, dependent: :destroy
     has_many :images
     accepts_nested_attributes_for :images, allow_destroy: true
@@ -8,4 +11,15 @@ class Item < ApplicationRecord
     belongs_to :size
     belongs_to :postage
     belongs_to :DeliveryDay
+    belongs_to_active_hash :prefecture
+
+    # バリデーション
+    validates :name,                presence: true
+    validates :description,         presence: true
+    validates :category_id,         presence: true
+    validates :price,               presence: true
+    validates :prefecture_id,       presence: true
+    validates :postage_id,          presence: true
+    validates :condition_id,        presence: true
+    validates :delivery_day_id,     presence: true
 end
