5cf69854601f418e77c415657e90a36afff2e7e3
tera-hana <htera711@vmail.plala.or.jp>
2020-03-24 17:41:10 +0900

    card関連のマイグレーションファイルの再作成

--- db/migrate/20200317130634_cards.rb
+++
@@ -1,5 +0,0 @@
-class Cards < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :cards
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,12 @@
 
 ActiveRecord::Schema.define(version: 20200317131205) do
 
+  create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "cards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id",     null: false
     t.string   "customer_id", null: false
@@ -26,6 +32,27 @@ ActiveRecord::Schema.define(version: 20200317131205) do
     t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
   end
 
+  create_table "category_sizes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "category_id"
+    t.integer  "size_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_category_sizes_on_category_id", using: :btree
+    t.index ["size_id"], name: "index_category_sizes_on_size_id", using: :btree
+  end
+
+  create_table "conditions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "condition",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "delivery_days", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "day",        null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "src"
     t.integer  "item_id"
@@ -34,34 +61,24 @@ ActiveRecord::Schema.define(version: 20200317131205) do
     t.index ["item_id"], name: "index_images_on_item_id", using: :btree
   end
 
-  create_table "installs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_installs_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true, using: :btree
-  end
-
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                          null: false
-    t.integer  "price",                         null: false
+    t.string   "name"
+    t.integer  "price"
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
-    t.string   "prefecture",                    null: false
+    t.string   "prefecture_id",                 null: false
+    t.string   "brand_id",                      null: false
     t.text     "description",     limit: 65535, null: false
-    t.integer  "condition_id",                  null: false
-    t.integer  "delivery_day_id",               null: false
-    t.integer  "brand_id",                      null: false
     t.integer  "likes_count",                   null: false
     t.integer  "user_id",                       null: false
     t.integer  "category_id",                   null: false
     t.integer  "item_state_id",                 null: false
     t.integer  "postage_id",                    null: false
+    t.integer  "condition_id"
+    t.integer  "delivery_day_id"
     t.index ["category_id"], name: "index_items_on_category_id", using: :btree
+    t.index ["condition_id"], name: "index_items_on_condition_id", using: :btree
+    t.index ["delivery_day_id"], name: "index_items_on_delivery_day_id", using: :btree
     t.index ["item_state_id"], name: "index_items_on_item_state_id", using: :btree
     t.index ["postage_id"], name: "index_items_on_postage_id", using: :btree
     t.index ["user_id"], name: "index_items_on_user_id", using: :btree
@@ -71,6 +88,20 @@ ActiveRecord::Schema.define(version: 20200317131205) do
     t.string   "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "postages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "delivery_charge", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "sizes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "size",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "ancestry"
+    t.index ["ancestry"], name: "index_sizes_on_ancestry", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -90,5 +121,7 @@ ActiveRecord::Schema.define(version: 20200317131205) do
     t.index ["tel"], name: "index_users_on_tel", unique: true, using: :btree
   end
 
+  add_foreign_key "category_sizes", "categories"
+  add_foreign_key "category_sizes", "sizes"
   add_foreign_key "images", "items"
 end
