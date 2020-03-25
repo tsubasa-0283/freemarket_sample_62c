57df72b41c3d1aa2f85bd034f666667a20e17408
tera-hana <htera711@vmail.plala.or.jp>
2020-03-25 18:46:52 +0900

    カードのモデルとコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,25 @@
 Rails.application.routes.draw do
+  get 'cards/new'
+  get 'cards/show'
+
   devise_for :users
   root "toppages#index"
   get 'users/new'
   root 'items#index'
+
   resources :items, except: :show
+  
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
+
+  resources :cards, only: [:new, :show, :destroy] do
+    collection do
+      post 'pay', to: 'cards#pay'
+    end
+  end
 end
