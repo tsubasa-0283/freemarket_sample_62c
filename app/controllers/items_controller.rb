7fa5d85148660fc9a48b2981ca33f003d1ed4c1d
tsubasa-0283 <tsubasaokamoto7@gmail.com>
2020-03-26 21:35:19 +0900

    none

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,55 +1,55 @@
 class ItemsController < ApplicationController
     before_action :set_item, only:[:destroy, :show, :edit, :update]
-    before_action :set_category, only: [:edit, :update, :create]
+      before_action :set_category, only: [:edit, :update, :create]
     def index
         @items = Items.includes(:images).order('create_at DESC')
     end
 
-    def new
-        @item = Item.new
-        @item.images.new
+      def new
+          @item = Item.new
+          @item.images.new
 
-        #セレクトボックスの初期値
-        @category_parent_array = Category.where(ancestry: nil)
-        # データベースから親カテゴリーのみを抽出し、配列化
-        # Category.where(ancestry: nil).each do |parent|
-        #   @category_parent_array << parent.name
-        # end
-    end
-    # 親カテゴリーが選択された後に動くアクション
-    def get_category_children
-      # 選択された親カテゴリーに紐づく子カテゴリーの配列を取得する
-      @category_children = Category.find_by(id: "#{params[:parent_id]}", ancestry: nil).children
-    end
-    # 子カテゴリーが選択された後に動くアクション
-    def get_category_grandchildren
-      # 選択された子カテゴリーに紐づく孫カテゴリーの配列を取得する
-      @category_grandchildren = Category.find("#{params[:child_id]}").children
-    end
+          #セレクトボックスの初期値
+          @category_parent_array = Category.where(ancestry: nil)
+          # データベースから親カテゴリーのみを抽出し、配列化
+          # Category.where(ancestry: nil).each do |parent|
+          #   @category_parent_array << parent.name
+          # end
+      end
+      # 親カテゴリーが選択された後に動くアクション
+      def get_category_children
+        # 選択された親カテゴリーに紐づく子カテゴリーの配列を取得する
+        @category_children = Category.find_by(id: "#{params[:parent_id]}", ancestry: nil).children
+      end
+      # 子カテゴリーが選択された後に動くアクション
+      def get_category_grandchildren
+        # 選択された子カテゴリーに紐づく孫カテゴリーの配列を取得する
+        @category_grandchildren = Category.find("#{params[:child_id]}").children
+      end
 
-    # 孫カテゴリーが選択された後に動くアクション
-    def get_size
-      selected_grandchild = Category.find("#{params[:grandchild_id]}") #孫カテゴリーを取得
-      if related_size_parent = selected_grandchild.sizes[0] #孫カテゴリーの親と紐づくサイズ（親）があれば取得
-        @sizes = related_size_parent.children #紐づいたサイズ（親）の子供の配列を取得する
-      else
-        selected_child = Category.find("#{params[:grandchildren_id]}").parent #孫カテゴリーの親を取得する
-        if related_size_parent = selected_child.sizes[0] #孫カテゴリーの親と紐づくサイズ（親）があれば取得
-          @sizes = related_size_parent.children #紐づいたサイズ（親）の子供の配列を取得
+      # 孫カテゴリーが選択された後に動くアクション
+      def get_size
+        selected_grandchild = Category.find("#{params[:grandchild_id]}") #孫カテゴリーを取得
+        if related_size_parent = selected_grandchild.sizes[0] #孫カテゴリーの親と紐づくサイズ（親）があれば取得
+          @sizes = related_size_parent.children #紐づいたサイズ（親）の子供の配列を取得する
+        else
+          selected_child = Category.find("#{params[:grandchildren_id]}").parent #孫カテゴリーの親を取得する
+          if related_size_parent = selected_child.sizes[0] #孫カテゴリーの親と紐づくサイズ（親）があれば取得
+            @sizes = related_size_parent.children #紐づいたサイズ（親）の子供の配列を取得
+          end
         end
       end
-    end
 
-    def create
-      @item = Item.new(item_params)
-      # @item.status = 0
-      @item.seller_id = "1"
-      if @item.save
-        redirect_to root_path, notice: "出品しました"
-      else
-        render :new unless @item.valid? alert: "必須項目を入力してください"
+      def create
+        @item = Item.new(item_params)
+        # @item.status = 0
+        @item.seller_id = "1"
+        if @item.save
+          redirect_to root_path, notice: "出品しました"
+        else
+          render :new unless @item.valid? alert: "必須項目を入力してください"
+        end
       end
-    end
 
     def edit
     end
@@ -99,9 +99,9 @@ class ItemsController < ApplicationController
       @item = Item.find(params[:id])
     end
 
-    def item_params
-      params.require(:item).permit(:name, :price, :description, :category_id, :size_id, :prefecture_id, :condition_id, :delivery_day_id, :postage_id, :brand_id, images_attributes: [:src] ).merge(seller_id: "1")
-    end
+      def item_params
+        params.require(:item).permit(:name, :price, :description, :category_id, :size_id, :prefecture_id, :condition_id, :delivery_day_id, :postage_id, :brand_id, images_attributes: [:src] ).merge(seller_id: "1")
+      end
 
     def item_update_params
       params.require(:item).permit(:name, :price, :description, :category_id, :size_id, :brand_id, :prefecture_id, :condition_id, :delivery_day_id, :postage_id )
@@ -115,8 +115,8 @@ class ItemsController < ApplicationController
       params.require(:new_images).permit({images: []})
     end
 
-    def set_category
-      @category_parent_array = Category.where(ancestry: nil)
-    end
+      def set_category
+        @category_parent_array = Category.where(ancestry: nil)
+      end
 
     
