ab32baf6a96543ea0c11ff9cb6267e380c0e55d3
tera-hana <htera711@vmail.plala.or.jp>
2020-04-08 01:02:47 +0900

    buyers_controller修正

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -6,14 +6,14 @@ class BuyersController < ApplicationController
     if @card.blank?
       redirect_to new_card_path
     else
-      Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(@card.customer_id) 
       @default_card_information = customer.cards.retrieve(@card.card_id)
     end
   end
 
   def pay
-    Payjp.api_key = Rails.application.credentials[:PAYJP_PRIVATE_KEY]
+    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
     Payjp::Charge.create(
       :amount => @item.price,
       :customer => @card.customer_id,
