93ff0d2128079c2a43c0fd400910aa3bbf8c26f0
mitsuhiro0602 <0hornet0@gmail.com>
2020-04-16 21:20:47 +0900

    ログアウト実装

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -17,12 +17,20 @@ class Users::SessionsController < Devise::SessionsController
 
   
 
+  # def destroy
+  #   session.delete(:user_id)                                                    # セッションのuser_idを削除する
+  #   @current_user = nil     
+  #   flash[:alert] = "ログアウトしました"
+  #   binding.pry
+  #   redirect_to root_path
+  # end
   def destroy
-    session.delete(:user_id)                                                    # セッションのuser_idを削除する
-    @current_user = nil     
-    flash[:alert] = "ログアウトしました"
-    binding.pry
-    redirect_to root_path
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :notice, :signed_out if signed_out
+    yield if block_given?
+    respond_to_on_destroy
   end
+    
+    
 end
 
