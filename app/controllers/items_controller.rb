bb2de44b1211e06a05ee5dea50a778cae6364b7a
tsubasa-0283 <tsubasaokamoto7@gmail.com>
2020-04-17 23:19:34 +0900

    エラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -78,7 +78,7 @@ class ItemsController < ApplicationController
         @smallcategory = Category.find(@item.category_id)
         @category = @smallcategory.parent # unless Category.find(@item.category_id)
         @bigcategory = @category.parent
-        @size = Size.find(@item.size_id)
+        # @size = Size.find(@item.size_id)
         @brand = Brand.find(@item.brand_id)
         @delivery = DeliveryDay.find(@item.delivery_day_id)
         @address = Prefecture.find(@item.prefecture_id)
